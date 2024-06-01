01c5ff83c995a0fb8221596b3a4a9508feb316f1
Nguyễn Văn Thành <thanh.nguyenvan@haravan.com>
2024-06-01 16:48:57 +0700

    up

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,63 @@
-import './App.css';
+import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
+import { createBrowserHistory } from 'history';
 import { Button, Input } from './components';
+// export const history = createBrowserHistory();
 
-function App() {
-  return (
-    <div className="App">
-      <div>
-        <Button />
-      </div>
-      <div>
-        <Input />
-      </div>
+// const RouteWithLayout = ({ component: Component, layout: Layout, ...rest }) => (
+//   <Route {...rest} render={props => (
+//     <Layout>
+//       <Component {...props} />
+//     </Layout>
+//   )} />
+// )
+
+const App = () => {
+  return <div className="App">
+    <div>
+      <Button />
     </div>
-  )
+    <div>
+      <Input />
+    </div>
+  </div>
+
+  // return <BrowserRouter>
+  //   <Routes>
+  //     <Route path="/" element={<Home />} />
+  //     <Route path="/about" element={<h2>About</h2>} />
+  //     <Route path="/blog/*" element={<BlogApp />} />
+  //     <Route path="/users/*" element={<UserApp />} />
+  //   </Routes>
+  // </BrowserRouter>
+
+  // function Home() {
+  //   return (
+  //     <>
+  //       <h1>Welcome!</h1>
+  //       <p>
+  //         Check out the <Link to="/blog">blog</Link> or the{" "}
+  //         <Link to="users">users</Link> section
+  //       </p>
+  //     </>
+  //   );
+  // }
+
+  // function BlogApp() {
+  //   return (
+  //     <Routes>
+  //       <Route index element={<h1>Blog Index</h1>} />
+  //       <Route path="posts" element={<h1>Blog Posts</h1>} />
+  //     </Routes>
+  //   );
+  // }
+
+  // function UserApp() {
+  //   return (
+  //     <Routes>
+  //       <Route index element={<h1>Users Index</h1>} />
+  //     </Routes>
+  //   );
+  // }
 }
 
 export default App;
