6d65075a818284a1289a87df6c61f0aaeb97c8a5
Nguyễn Văn Thành <thanh.nguyenvan@haravan.com>
2024-06-01 18:17:42 +0700

    up

--- src/App.tsx
+++ src/App.tsx
@@ -1,63 +1,78 @@
-import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
-import { createBrowserHistory } from 'history';
+import { Link, NavLink, Outlet, Route, Routes } from 'react-router-dom';
 import { Button, Input } from './components';
-// export const history = createBrowserHistory();
-
-// const RouteWithLayout = ({ component: Component, layout: Layout, ...rest }) => (
-//   <Route {...rest} render={props => (
-//     <Layout>
-//       <Component {...props} />
-//     </Layout>
-//   )} />
-// )
+import './App.css';
 
 const App = () => {
-  return <div className="App">
-    <div>
-      <Button />
-    </div>
-    <div>
-      <Input />
-    </div>
-  </div>
-
-  // return <BrowserRouter>
-  //   <Routes>
-  //     <Route path="/" element={<Home />} />
-  //     <Route path="/about" element={<h2>About</h2>} />
-  //     <Route path="/blog/*" element={<BlogApp />} />
-  //     <Route path="/users/*" element={<UserApp />} />
-  //   </Routes>
-  // </BrowserRouter>
-
-  // function Home() {
-  //   return (
-  //     <>
-  //       <h1>Welcome!</h1>
-  //       <p>
-  //         Check out the <Link to="/blog">blog</Link> or the{" "}
-  //         <Link to="users">users</Link> section
-  //       </p>
-  //     </>
-  //   );
-  // }
-
-  // function BlogApp() {
-  //   return (
-  //     <Routes>
-  //       <Route index element={<h1>Blog Index</h1>} />
-  //       <Route path="posts" element={<h1>Blog Posts</h1>} />
-  //     </Routes>
-  //   );
-  // }
-
-  // function UserApp() {
-  //   return (
-  //     <Routes>
-  //       <Route index element={<h1>Users Index</h1>} />
-  //     </Routes>
-  //   );
-  // }
+  return <Routes>
+    <Route path="/" element={<Layout />}>
+      <Route path="/" element={<div>Home</div>} />
+      <Route path="/button" element={<Button />} />
+      <Route path="/input" element={<Input />} />
+      <Route path="*" element={<NoMatch />} />
+    </Route>
+  </Routes>
 }
 
 export default App;
+
+const Layout = (props: any) => {
+  return <div className='container-fluid'>
+    <Header />
+    <div className='main-wrapper'>
+      <div className='layout layout-has-sider'>
+        <Nav />
+        <main className='layout-content'> <Outlet /></main>
+      </div>
+    </div>
+  </div>
+}
+
+const Header = () => {
+  return <header className='header-page'>
+    <div className='row align-items-center'>
+      <div className='col-8'>
+        <ul className='header-mainmenu'>
+          <li>
+            <NavLink to={'/'}>Home</NavLink>
+          </li>
+          <li>
+            <NavLink to={'/changelog'}>ChangeLog</NavLink>
+          </li>
+          <li>
+            <NavLink to={'/designs'}>Designs</NavLink>
+          </li>
+          <li>
+            <NavLink to={'/components'}>Components</NavLink>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </header>
+}
+
+const Nav = () => {
+  return <div className='layout-sider'>
+    <ul className='main-menu-container'>
+      <li className='nav-menu-item'>
+        <Link to={'/'} className='nav-menu-text'>Home</Link>
+      </li>
+      <li className='nav-menu-item'>
+        <Link to={'/button'} className='nav-menu-text'>Button</Link>
+      </li>
+      <li className='nav-menu-item'>
+        <Link to={'/input'} className='nav-menu-text'>Input</Link>
+      </li>
+    </ul>
+  </div>
+}
+
+function NoMatch() {
+  return (
+    <div>
+      <h2>Nothing to see here!</h2>
+      <p>
+        <Link to="/">Go to the home page</Link>
+      </p>
+    </div>
+  )
+}
